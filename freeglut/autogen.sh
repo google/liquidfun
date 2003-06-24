f52a5e44ad0e41704419647c5c94e64b947b3b4b
andi75 <andi75@7f0cb862-5218-0410-a997-914c9d46530a>
2003-06-24 18:12:40 +0000

    fixed aclocal / autoheader order; some cosmetics
    git-svn-id: http://svn.code.sf.net/p/freeglut/code/trunk/freeglut/freeglut@100 7f0cb862-5218-0410-a997-914c9d46530a

--- freeglut/autogen.sh
+++ freeglut/autogen.sh
@@ -1,6 +1,12 @@
-set  -x                      &&
-autoheader                   &&
-aclocal                      &&
-libtoolize  --copy  --force  &&
-automake  --add-missing      &&
+#!/bin/sh
+#
+echo "Generating build information using aclocal, automake and autoconf"
+echo "This may take a while ..."
+
+aclocal
+autoheader
+libtoolize  --copy  --force
+automake  --add-missing
 autoconf
+
+echo "Now you are ready to run ./configure"
