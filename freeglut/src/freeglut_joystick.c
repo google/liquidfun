5fbd8a6505792d017aac157662d9d96800026ed8
spanne <spanne@7f0cb862-5218-0410-a997-914c9d46530a>
2005-07-02 13:13:30 +0000

    Partial fixes for bug #1218900 (freeglut-2.4.0 on FreeBSD).
    git-svn-id: http://svn.code.sf.net/p/freeglut/code/trunk/freeglut/freeglut@660 7f0cb862-5218-0410-a997-914c9d46530a

--- freeglut/src/freeglut_joystick.c
+++ freeglut/src/freeglut_joystick.c
@@ -80,7 +80,7 @@
 /* XXX The below hack is done until freeglut's autoconf is updated. */
 #        define HAVE_USB_JS    1
 
-#        if defined(__FreeBSD__) && __FreeBSD_version >= 500000
+#        if defined(__FreeBSD__)
 #            include <sys/joystick.h>
 #        else
 /*
@@ -656,9 +656,9 @@ static void fghJoystickRawRead( SFG_Joystick* joy, int* buttons, float* axes )
                if (usage > 0 && usage < _JS_MAX_BUTTONS + 1)
                {
                    if (d)
-                       joy->os->cache_buttons |= (1 << usage - 1);
+                       joy->os->cache_buttons |=  (1 << ( usage - 1 ));
                    else
-                       joy->os->cache_buttons &= ~(1 << usage - 1);
+                       joy->os->cache_buttons &= ~(1 << ( usage - 1 ));
                }
             }
         }
@@ -1060,7 +1060,9 @@ static void fghJoystickOpen( SFG_Joystick* joy )
 #    ifdef JS_NEW
        unsigned char u;
 #    else
-       int counter;
+#      if defined( __linux__ )
+         int counter;
+#      endif
 #    endif
 #endif
 
