9ed420fbcc7b4fd727d38d7a6734e1fa317c23fe
fayjf <fayjf@7f0cb862-5218-0410-a997-914c9d46530a>
2005-07-13 20:25:07 +0000

    Fixing a cursor bug in \"GLUT_CURSOR_INHERIT\"
    git-svn-id: http://svn.code.sf.net/p/freeglut/code/trunk/freeglut/freeglut@677 7f0cb862-5218-0410-a997-914c9d46530a

--- freeglut/src/freeglut_cursor.c
+++ freeglut/src/freeglut_cursor.c
@@ -147,11 +147,13 @@ void fgSetCursor ( SFG_Window *window, int cursorID )
             }
         }
 
-        if ( ( cursorIDToUse != GLUT_CURSOR_NONE ) && ( cursor == None ) ) {
+        if ( cursorIDToUse == GLUT_CURSOR_INHERIT )
+            XUndefineCursor( fgDisplay.Display, window->Window.Handle );
+        if ( ( cursorIDToUse != GLUT_CURSOR_NONE ) && ( cursor == None ) )
             fgError( "Failed to create cursor" );
-        }
-        XDefineCursor( fgDisplay.Display,
-                       window->Window.Handle, cursor );
+        else
+            XDefineCursor( fgDisplay.Display,
+                           window->Window.Handle, cursor );
     }
 
 #elif TARGET_HOST_WIN32 || TARGET_HOST_WINCE
