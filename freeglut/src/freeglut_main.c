8f76d5ec73c2d4655c078ec81511f0dc9af8f545
spanne <spanne@7f0cb862-5218-0410-a997-914c9d46530a>
2008-11-06 19:17:13 +0000

    Ooops, forgot one file in the previous commit (fix for "--without-x").
    git-svn-id: http://svn.code.sf.net/p/freeglut/code/trunk/freeglut/freeglut@759 7f0cb862-5218-0410-a997-914c9d46530a

--- freeglut/src/freeglut_main.c
+++ freeglut/src/freeglut_main.c
@@ -308,7 +308,7 @@ static void fghCheckTimers( void )
  * 32-bit, where the GLUT API return value is also overflowed.
  */  
 unsigned long fgSystemTime(void) {
-#if TARGET_HOST_POSIX_X11
+#if HAVE_GETTIMEOFDAY
     struct timeval now;
     gettimeofday( &now, NULL );
     return now.tv_usec/1000 + now.tv_sec*1000;
