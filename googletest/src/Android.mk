a11ed1f94092a82ad7b0db4bf41c04ee54ad9e92
Nicolas Catania <niko@google.com>
2009-04-30 14:02:25 -0700

    First import of gtest 1.3.0
    Removed unecessary make files for other systems.
    Added Android.mk to build the libraries and test suite on host.
    
    Will enable the target build when the aSTL is available.
    
    Added NOTICE and MODULE_LICENSE files.
    
    Tests are tagged with 'eng' and 'tests'

---
+++ googletest/src/Android.mk
@@ -0,0 +1,71 @@
+# Copyright (C) 2009 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+
+LOCAL_PATH := $(call my-dir)
+
+#######################################################################
+# gtest lib host
+
+include $(CLEAR_VARS)
+
+LOCAL_CPP_EXTENSION := .cc
+# TODO: may need to drag these in a shared variable when we start to
+# support target builds.
+LOCAL_SRC_FILES := \
+    gtest.cc \
+    gtest-death-test.cc \
+    gtest-filepath.cc \
+    src/gtest-internal-inl.h \
+    gtest-port.cc \
+    gtest-test-part.cc \
+    gtest-typed-test.cc
+
+
+LOCAL_C_INCLUDES := \
+    $(LOCAL_PATH)/.. \
+    $(LOCAL_PATH)/../include
+
+
+LOCAL_CFLAGS += -DGTEST_OS_LINUX -DGTEST_HAS_STD_STRING -O0 -g
+
+LOCAL_MODULE := libgtest
+LOCAL_MODULE_TAGS := eng tests
+
+include $(BUILD_HOST_STATIC_LIBRARY)
+
+#######################################################################
+# gtest_main lib host
+
+include $(CLEAR_VARS)
+
+LOCAL_CPP_EXTENSION := .cc
+# TODO: may need to drag these in a shared variable when we start to
+# support target builds.
+LOCAL_SRC_FILES := \
+    gtest_main.cc
+
+LOCAL_C_INCLUDES := \
+    $(LOCAL_PATH)/.. \
+    $(LOCAL_PATH)/../include
+
+LOCAL_CFLAGS += -DGTEST_OS_LINUX -DGTEST_HAS_STD_STRING -O0 -g
+
+LOCAL_STATIC_LIBRARIES := libgtest
+
+LOCAL_MODULE := libgtest_main
+LOCAL_MODULE_TAGS := eng tests
+
+include $(BUILD_HOST_STATIC_LIBRARY)
