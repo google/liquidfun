a2a900f91ec18963bdd11c7fd59dbbea5d083552
fayjf <fayjf@7f0cb862-5218-0410-a997-914c9d46530a>
2012-02-12 04:41:32 +0000

    Moving some Windows-specific preprocessor stuff and a prototype from "freeglut_init.c" to the Windows header file
    git-svn-id: http://svn.code.sf.net/p/freeglut/code/trunk/freeglut/freeglut@1077 7f0cb862-5218-0410-a997-914c9d46530a

--- freeglut/src/Common/freeglut_init.c
+++ freeglut/src/Common/freeglut_init.c
@@ -314,24 +314,6 @@ void fgDeinitialize( void )
 }
 
 
-#if TARGET_HOST_MS_WINDOWS
-#define NoValue         0x0000
-#define XValue          0x0001
-#define YValue          0x0002
-#define WidthValue      0x0004
-#define HeightValue     0x0008
-#define AllValues       0x000F
-#define XNegative       0x0010
-#define YNegative       0x0020
-
-extern int XParseGeometry (
-    const char *string,
-    int *x,
-    int *y,
-    unsigned int *width,    /* RETURN */
-    unsigned int *height);    /* RETURN */
-#endif
-
 /* -- INTERFACE FUNCTIONS -------------------------------------------------- */
 
 /*
--- freeglut/src/mswin/freeglut_internal_mswin.h
+++ freeglut/src/mswin/freeglut_internal_mswin.h
@@ -95,6 +95,25 @@ struct tagSFG_PlatformWindowState
 
 
 
+/* For "freeglut_init.c", some definitions of symbols and a prototype to emulate the X11 functionality */
+#define NoValue         0x0000
+#define XValue          0x0001
+#define YValue          0x0002
+#define WidthValue      0x0004
+#define HeightValue     0x0008
+#define AllValues       0x000F
+#define XNegative       0x0010
+#define YNegative       0x0020
+
+extern int XParseGeometry (
+    const char *string,
+    int *x,
+    int *y,
+    unsigned int *width,    /* RETURN */
+    unsigned int *height);    /* RETURN */
+
+
+
 /* Joystick-Specific Definitions */
 #if !defined(_WIN32_WCE)
 #    define _JS_MAX_AXES  8
