1ed0d43353e7a6fe7be5069e19538f50b94194d4
fayjf <fayjf@7f0cb862-5218-0410-a997-914c9d46530a>
2012-02-25 05:47:57 +0000

    Implementing the monotonic clock if available, per e-mail from Phil Vandry dated 2/17/12 at 5:08 PM
    git-svn-id: http://svn.code.sf.net/p/freeglut/code/trunk/freeglut/freeglut@1079 7f0cb862-5218-0410-a997-914c9d46530a

--- freeglut/src/x11/freeglut_main_x11.c
+++ freeglut/src/x11/freeglut_main_x11.c
@@ -91,10 +91,16 @@ void fgPlatformDisplayWindow ( SFG_Window *window )
 
 unsigned long fgPlatformSystemTime ( void )
 {
+#ifdef CLOCK_MONOTONIC
+    struct timespec now;
+    clock_gettime(CLOCK_MONOTONIC, &now);
+    return now.tv_nsec/1000000 + now.tv_sec*1000;
+#else
 #ifdef HAVE_GETTIMEOFDAY
     struct timeval now;
     gettimeofday( &now, NULL );
     return now.tv_usec/1000 + now.tv_sec*1000;
+#endif
 #endif
 }
 
