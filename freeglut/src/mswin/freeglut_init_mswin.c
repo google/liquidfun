099bc381a160326d6e2438bb34459705539feb48
beuc <beuc@7f0cb862-5218-0410-a997-914c9d46530a>
2012-03-09 20:54:03 +0000

    Make XParseGeometry reusable by several ports (3)
    git-svn-id: http://svn.code.sf.net/p/freeglut/code/trunk/freeglut/freeglut@1093 7f0cb862-5218-0410-a997-914c9d46530a

--- freeglut/src/mswin/freeglut_init_mswin.c
+++ freeglut/src/mswin/freeglut_init_mswin.c
@@ -163,38 +163,6 @@ void fgPlatformDestroyContext ( SFG_PlatformDisplay pDisplay, SFG_WindowContextT
 	/* Do nothing -- this is required for X11 */
 }
 
-/*
- * Everything down to the end of the next two functions is copied from the X sources.
- */
-
-/*
-
-Copyright 1985, 1986, 1987,1998  The Open Group
-
-Permission to use, copy, modify, distribute, and sell this software and its
-documentation for any purpose is hereby granted without fee, provided that
-the above copyright notice appear in all copies and that both that
-copyright notice and this permission notice appear in supporting
-documentation.
-
-The above copyright notice and this permission notice shall be included
-in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-IN NO EVENT SHALL THE OPEN GROUP BE LIABLE FOR ANY CLAIM, DAMAGES OR
-OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-OTHER DEALINGS IN THE SOFTWARE.
-
-Except as contained in this notice, the name of The Open Group shall
-not be used in advertising or otherwise to promote the sale, use or
-other dealings in this Software without prior written authorization
-from The Open Group.
-
-*/
-
 /* -- PLATFORM-SPECIFIC INTERFACE FUNCTION -------------------------------------------------- */
 
 void (__cdecl *__glutExitFunc)( int return_value ) = NULL;
