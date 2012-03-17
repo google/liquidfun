050c0514db6121eb867ff07c5d060e16ab7a1967
beuc <beuc@7f0cb862-5218-0410-a997-914c9d46530a>
2012-03-17 14:18:33 +0000

    Add 2 more temporary stubs
    git-svn-id: http://svn.code.sf.net/p/freeglut/code/trunk/freeglut/freeglut@1181 7f0cb862-5218-0410-a997-914c9d46530a

--- freeglut/src/gles_stubs.c
+++ freeglut/src/gles_stubs.c
@@ -7,3 +7,10 @@ void fgDeactivateMenu( SFG_Window *window ) {
 void fgDisplayMenu( void ) {
   fprintf(stderr, "fgDisplayMenu: STUB\n");
 }
+void fgUpdateMenuHighlight ( SFG_Menu *menu ) {
+  fprintf(stderr, "fgUpdateMenuHighlight: STUB\n");
+}
+GLboolean fgCheckActiveMenu ( SFG_Window *window, int button, GLboolean pressed,
+                              int mouse_x, int mouse_y )  {
+  fprintf(stderr, "fgCheckActiveMenu: STUB\n");
+}
