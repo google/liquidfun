68d6eecb43212c3591b656d4b6070b6eaecb1a97
beuc <beuc@7f0cb862-5218-0410-a997-914c9d46530a>
2012-04-02 22:22:33 +0000

    Use different names for GLES1 and GLES2 libraries, to allow parallel install
    git-svn-id: http://svn.code.sf.net/p/freeglut/code/trunk/freeglut/freeglut@1233 7f0cb862-5218-0410-a997-914c9d46530a

--- freeglut/android/gles2/Android.mk
+++ freeglut/android/gles2/Android.mk
@@ -2,8 +2,8 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := freeglut
-LOCAL_SRC_FILES := lib/libglut.a
+LOCAL_MODULE := freeglut-gles2
+LOCAL_SRC_FILES := lib/libfreeglut-gles2.a
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
 
 include $(PREBUILT_STATIC_LIBRARY)
