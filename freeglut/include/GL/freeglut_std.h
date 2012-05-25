a39678aee8c270320a213430e4e3b1015a8ab028
dcnieho <dcnieho@7f0cb862-5218-0410-a997-914c9d46530a>
2012-05-25 01:31:25 +0000

    on windows+MSVC, debug builds of the library now have a 'd' suffix. freeglutdll.def was edited to no longer include the name of teh library that would clash with this and freeglut_std.h was edited to specify linking to the right library when building in debug mode
    git-svn-id: http://svn.code.sf.net/p/freeglut/code/trunk/freeglut/freeglut@1322 7f0cb862-5218-0410-a997-914c9d46530a

--- freeglut/include/GL/freeglut_std.h
+++ freeglut/include/GL/freeglut_std.h
@@ -70,7 +70,11 @@
 
         /* Link with Win32 static freeglut lib */
 #       if FREEGLUT_LIB_PRAGMAS
-#           pragma comment (lib, "freeglut_static.lib")
+#           ifdef NDEBUG
+#              pragma comment (lib, "freeglut_static.lib")
+#           else
+#              pragma comment (lib, "freeglut_staticd.lib")
+#           endif
 #       endif
 
 /* Windows shared library (DLL) */
@@ -84,7 +88,11 @@
 
             /* Link with Win32 shared freeglut lib */
 #           if FREEGLUT_LIB_PRAGMAS
-#               pragma comment (lib, "freeglut.lib")
+#               ifdef NDEBUG
+#                   pragma comment (lib, "freeglut.lib")
+#               else
+#                   pragma comment (lib, "freeglutd.lib")
+#               endif
 #           endif
 
 #       endif
