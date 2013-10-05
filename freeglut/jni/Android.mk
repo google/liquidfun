07017bf1d9d1b17bc4bde678546e547c193b19a1
Stewart Miles <smiles@google.com>
2013-10-04 18:15:20 -0700

    Added Android NDK makefiles for freeglut so that it's possible to avoid cmake.
    Tested:
    $ cd vendor/unbundled_google/libs/freeglut
    $ ndk-build -j16
    .. lots of build output ..
    Verified build output looks sane.
    
    Bug: 11043701
    Change-Id: I5dd33294f025c79e63309ac62b8f5310fce08288

---
+++ freeglut/jni/Android.mk
@@ -0,0 +1,58 @@
+LOCAL_PATH:= $(call my-dir)/..
+
+define build-freeglut
+${eval \
+  LOCAL_MODULE:=freeglut-gles$1$2
+  LOCAL_SRC_FILES:=\
+    $(filter-out $(LOCAL_PATH)/src/fg_font.c \
+                 $(LOCAL_PATH)/src/fg_menu.c,\
+      $(wildcard $(LOCAL_PATH)/src/*.c)) \
+    $(wildcard $(LOCAL_PATH)/src/android/*.c) \
+    $(wildcard $(LOCAL_PATH)/src/android/native_app_glue/*.c) \
+    $(wildcard $(LOCAL_PATH)/src/egl/*.c) \
+    $(wildcard $(LOCAL_PATH)/src/util/*c)
+  LOCAL_C_INCLUDES:=\
+    $(LOCAL_PATH)/src \
+    $(LOCAL_PATH)/include
+  LOCAL_CFLAGS:=-DFREEGLUT_GLES$1 \
+                -Dbool=int \
+                -Dfalse=0 \
+                -D"true=(!false)" \
+                -DNEED_XPARSEGEOMETRY_IMPL
+  LOCAL_CFLAGS+=\
+    ${foreach v,VERSION_MAJOR VERSION_MINOR VERSION_PATCH,\
+        -D${v}=${shell awk '/${v}/ { s = $$2; sub(/\)/, "", s); print s }' \
+                   $(LOCAL_PATH)/CMakeLists.txt}}
+  ifeq ($1,1)
+    gllibsuffix:=_CM
+  else
+    gllibsuffix:=
+  endif
+  libraries:=log android EGL GLESv$1$$(gllibsuffix)
+  ifeq ($2,_static)
+    LOCAL_STATIC_LIBRARIES:=$$(libraries)
+  else
+    LOCAL_LDLIBS:=$$(foreach l,$$(libraries),-l$$(l))
+    # Allow runtime linking of main().
+    LOCAL_ALLOW_UNDEFINED_SYMBOLS:=true
+    # Export symbols.
+    LOCAL_CFLAGS+=-DFREEGLUT_EXPORTS
+  endif
+  LOCAL_EXPORT_C_INCLUDES:=$(LOCAL_PATH)/include}
+endef
+
+include $(CLEAR_VARS)
+$(call build-freeglut,1,_static)
+include $(BUILD_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+$(call build-freeglut,1,)
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+$(call build-freeglut,2,_static)
+include $(BUILD_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+$(call build-freeglut,2,)
+include $(BUILD_SHARED_LIBRARY)
