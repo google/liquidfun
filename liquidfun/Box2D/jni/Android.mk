76def8a050f1d7aace7bd5bb66d9b42d26a8394f
Stewart Miles <smiles@google.com>
2013-10-08 13:40:09 -0700

    Removed cmake wrapper makefile from Box2D's build process.
    Tested:
    
    Verified that it's still possible to build box2d using the Android and NDK
    build processes.
    
    Change-Id: I694da1b38ba9fc212fcbb89d40cc93b363a952c4

--- liquidfun/Box2D/jni/Android.mk
+++ liquidfun/Box2D/jni/Android.mk
@@ -1,9 +1,5 @@
 LOCAL_PATH:=$(call my-dir)/..
 
-# Whether to build using box2d's cmake project or use Android's build system
-# directly.
-BOX2D_BUILD_USING_CMAKE?=0
-
 # Conditionally include libstlport (so include path is added to CFLAGS) if
 # it's not being built using the NDK build process.
 define add-stlport-includes
@@ -22,9 +18,6 @@ $(eval \
   LOCAL_COPY_HEADERS_TO:=Box2D$(1))
 endef
 
-# === Build using the standard Android build process. ===
-ifeq ($(BOX2D_BUILD_USING_CMAKE),0)
-
 # Execute a shell command relative to this module's directory.
 define execute-local
 $(patsubst ./%,%,$(shell cd $(LOCAL_PATH) ; eval "$(1)"))
@@ -39,8 +32,6 @@ $(eval \
     $(call execute-local, "find Box2D -name '*.cpp'")
   LOCAL_COPY_HEADERS:=\
     $(call execute-local, "find Box2D -name '*.h'")
-# May need this for Android build process.
-#  LOCAL_C_INCLUDES:=$(LOCAL_PATH)
   LOCAL_EXPORT_C_INCLUDES:=$(LOCAL_C_INCLUDES)
   $$(call add-stlport-includes))
 endef
@@ -56,40 +47,3 @@ include $(BUILD_SHARED_LIBRARY)
 include $(CLEAR_VARS)
 $(call box2d-build,_static)
 include $(BUILD_STATIC_LIBRARY)
-
-else  # BOX2D_BUILD_USING_CMAKE
-
-# Configure local variables to build box2d using cmake, adding $(1) to the end
-# of the build target's name and $(2) to cmake's options.
-define box2d-build
-$(eval \
-  $$(call box2d-module,$(1))
-  # Configure box2d.
-  LOCAL_CMAKE_OPTIONS:=\
-    -DBOX2D_INSTALL=ON \
-    -DBOX2D_INSTALL_DOC=OFF \
-    -DBOX2D_BUILD_EXAMPLES=OFF \
-    -DBOX2D_BUILD_UNITTESTS=OFF \
-    -DLIB_INSTALL_DIR="." \
-    $(2)
-  # Path of the source project relative to LOCAL_PATH.
-  LOCAL_CMAKE_PROJECT_PATH:=.
-  # Path containing headers to be redistributed with the library.
-  LOCAL_CMAKE_HEADER_INSTALL_PATH:=include
-  $$(call add-stlport-includes))
-endef
-
-# --- libBox2D ---
-include $(CLEAR_VARS)
-$(call box2d-build,,-DBOX2D_BUILD_STATIC=OFF -DBOX2D_BUILD_SHARED=ON)
-LOCAL_CMAKE_INCLUDE_RULES:=$(BUILD_SHARED_LIBRARY)
-include $(LOCAL_PATH)/jni/cmake.mk
-
-# --- libBox2D_static ---
-include $(CLEAR_VARS)
-$(call box2d-build,_static,-DBOX2D_BUILD_STATIC=ON -DBOX2D_BUILD_SHARED=OFF)
-LOCAL_CMAKE_MODULE_OVERRIDE:=libBox2D
-LOCAL_CMAKE_INCLUDE_RULES:=$(BUILD_STATIC_LIBRARY)
-include $(LOCAL_PATH)/jni/cmake.mk
-
-endif  # BOX2D_BUILD_USING_CMAKE
