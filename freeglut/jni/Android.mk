c1572f52966e04f77f2d31fa2d0c020b612cb589
Stewart Miles <smiles@google.com>
2013-11-01 18:31:46 -0700

    Modified NDK makefile to build using ndk-build.cmd on Windows.
    Paths referenced via LOCAL_SRC_FILES have been changed so they're be relative
    to the directory above Android.mk on Windows, otherwise the ndk-build.cmd fails
    with the following error...
    
    android-ndk-r9/build/core/build-binary.mk:345: *** target pattern contains no `%'.  Stop.
    
    This includes refactoring CMakeLists.txt to move the version number declartion
    into veresion.mk so that Windows doesn't require awk to parse the
    CMakeLists.txt file for freeglut's version number.
    
    In addition, updated the AndroidManifest.xml so that it's valid XML.
    
    Tested:
    Verified freeglut builds natively on Linux, Windows and OSX and cross compiles
    for Android on Windows and Linux.
    
    Bug: 11444617
    Change-Id: I2f90ac273b04af57987cf7fcceb11f3ca05601bc

--- freeglut/jni/Android.mk
+++ freeglut/jni/Android.mk
@@ -1,25 +1,27 @@
 LOCAL_PATH:= $(call my-dir)/..
 
+include $(LOCAL_PATH)/version.mk
+
 define build-freeglut
 ${eval \
   LOCAL_MODULE:=freeglut-gles$1$2
   LOCAL_SRC_FILES:=\
-    $(filter-out $(LOCAL_PATH)/src/fg_font.c \
-                 $(LOCAL_PATH)/src/fg_menu.c,\
-      $(wildcard $(LOCAL_PATH)/src/*.c)) \
-    $(wildcard $(LOCAL_PATH)/src/android/*.c) \
-    $(wildcard $(LOCAL_PATH)/src/android/native_app_glue/*.c) \
-    $(wildcard $(LOCAL_PATH)/src/egl/*.c) \
-    $(wildcard $(LOCAL_PATH)/src/util/*c)
+    $(subst $(LOCAL_PATH)/,,\
+      $(filter-out $(LOCAL_PATH)/src/fg_font.c \
+                   $(LOCAL_PATH)/src/fg_menu.c,\
+        $(wildcard $(LOCAL_PATH)/src/*.c)) \
+      $(wildcard $(LOCAL_PATH)/src/android/*.c) \
+      $(wildcard $(LOCAL_PATH)/src/android/native_app_glue/*.c) \
+      $(wildcard $(LOCAL_PATH)/src/egl/*.c) \
+      $(wildcard $(LOCAL_PATH)/src/util/*c))
   LOCAL_C_INCLUDES:=\
     $(LOCAL_PATH)/src \
     $(LOCAL_PATH)/include
   LOCAL_CFLAGS:=-DFREEGLUT_GLES$1 \
-                -DNEED_XPARSEGEOMETRY_IMPL
-  LOCAL_CFLAGS+=\
-    ${foreach v,VERSION_MAJOR VERSION_MINOR VERSION_PATCH,\
-        -D${v}=${shell awk '/${v}/ { s = $$2; sub(/\)/, "", s); print s }' \
-                   $(LOCAL_PATH)/CMakeLists.txt}}
+                -DNEED_XPARSEGEOMETRY_IMPL \
+                -DVERSION_MAJOR=$(VERSION_MAJOR) \
+                -DVERSION_MINOR=$(VERSION_MINOR) \
+                -DVERSION_PATCH=$(VERSION_PATCH)
   ifeq ($1,1)
     gllibsuffix:=_CM
   else
