7d9149f6d36dd21c84f54b8ea0b9179ecd67cf22
Stewart Miles <smiles@google.com>
2013-11-27 11:44:13 -0800

    Workaround the introduction of the liquidfun mirror project.
    This is a temporary work around the liquidfun project being present in the
    tree which results in multiple libliquidfun and libliquidfun_static build
    targets being visible to the Android build system.
    
    Tested:
    Verified that it's possible to build everything under the splash2d and
    liquidfun projects using the Android build system.
    
    $ bash
    $ source build/envsetup.sh
    $ tapas nada
    $ make libsplash2d libsplash2d_static libliquidfun libliquidfun_static
    .. success ..
    
    Bug: 11880022
    Change-Id: I5b66e08ba4bd4dfc07c86ba68e495d1e16b408e4

---
+++ liquidfun/Box2D/Android.mk
@@ -0,0 +1,21 @@
+# Copyright (c) 2013 Google, Inc.
+#
+# This software is provided 'as-is', without any express or implied
+# warranty.  In no event will the authors be held liable for any damages
+# arising from the use of this software.
+# Permission is granted to anyone to use this software for any purpose,
+# including commercial applications, and to alter it and redistribute it
+# freely, subject to the following restrictions:
+# 1. The origin of this software must not be misrepresented; you must not
+# claim that you wrote the original software. If you use this software
+# in a product, an acknowledgment in the product documentation would be
+# appreciated but is not required.
+# 2. Altered source versions must be plainly marked as such, and must not be
+# misrepresented as being the original software.
+# 3. This notice may not be removed or altered from any source distribution.
+
+# NOTE: This is a temporary change that forces the Android build system (not
+# NDK) to only include the library build targets, all examples and supporting
+# library makefiles are ignored.
+LOCAL_PATH:=$(call my-dir)
+include $(LOCAL_PATH)/jni/Android.mk
