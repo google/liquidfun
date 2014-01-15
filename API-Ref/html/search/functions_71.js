8be2201847fed8ba976b07f5a4272c7458ecb7e3
Stewart Miles <smiles@google.com>
2014-01-15 13:13:36 -0800

    Regenerated HTML reference after recent API changes.
    Tested:
    Glanced at the API ref in a browser and it looks ok.
    
    Change-Id: I51e7397d2e6e3edacf08bbacc4c598a37508ab3a

--- API-Ref/html/search/functions_71.js
+++ API-Ref/html/search/functions_71.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['query',['Query',['../classb2_broad_phase.html#aea5732362c8bb3959c6b24f967654d93',1,'b2BroadPhase::Query()'],['../classb2_dynamic_tree.html#adf70aee89b4692fc79d65b1f54308585',1,'b2DynamicTree::Query()']]],
-  ['queryaabb',['QueryAABB',['../classb2_world.html#a711e55d2c6e68400f93472f807c3775b',1,'b2World']]]
+  ['queryaabb',['QueryAABB',['../classb2_world.html#a711e55d2c6e68400f93472f807c3775b',1,'b2World']]],
+  ['queryshapeaabb',['QueryShapeAABB',['../classb2_world.html#a8b05f1812b65fb1540dd2054a10a8da6',1,'b2World']]]
 ];
