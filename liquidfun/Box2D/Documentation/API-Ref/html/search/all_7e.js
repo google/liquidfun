14e7807ed6bc018f06b770f73ec4f6f06835c8da
Stewart Miles <smiles@google.com>
2014-03-04 12:35:31 -0800

    Bought release notes up to date with recent changes.
    Tested:
    Rebuilt HTML docs and verified that the release notes are formatted correctly.
    
    Change-Id: If1322c0c04e85904ca4ce7c492531235df412a7f

--- liquidfun/Box2D/Documentation/API-Ref/html/search/all_7e.js
+++ liquidfun/Box2D/Documentation/API-Ref/html/search/all_7e.js
@@ -4,6 +4,7 @@ var searchData=
   ['_7eb2dynamictree',['~b2DynamicTree',['../classb2_dynamic_tree.html#a9060565fc63b4dd87d9560775c076786',1,'b2DynamicTree']]],
   ['_7eb2freelist',['~b2FreeList',['../classb2_free_list.html#a80eb1a4823b06dfb416edb8d62b4990f',1,'b2FreeList']]],
   ['_7eb2intrusivelistnode',['~b2IntrusiveListNode',['../classb2_intrusive_list_node.html#a6f66f51630fa5caa702ecea85f1ef8d4',1,'b2IntrusiveListNode']]],
+  ['_7eb2particlehandle',['~b2ParticleHandle',['../classb2_particle_handle.html#a9a5bfd25fd59eaf9628b942ccfaec19a',1,'b2ParticleHandle']]],
   ['_7eb2slaballocator',['~b2SlabAllocator',['../classb2_slab_allocator.html#ab59d1761fb13142c034ab83c15888f1a',1,'b2SlabAllocator']]],
   ['_7eb2trackedblockallocator',['~b2TrackedBlockAllocator',['../classb2_tracked_block_allocator.html#a5424d35f8adad967e12f14741f4e71cb',1,'b2TrackedBlockAllocator']]],
   ['_7eb2typedfreelist',['~b2TypedFreeList',['../classb2_typed_free_list.html#a3ae4d61a6e3b193e0778ad3b47a22ac3',1,'b2TypedFreeList']]],
