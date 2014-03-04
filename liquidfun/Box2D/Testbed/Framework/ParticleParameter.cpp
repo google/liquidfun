9face617a5d635bd173528e3fe7659c93fb44df8
Stewart Miles <smiles@google.com>
2014-03-04 14:03:34 -0800

    Moved the bounding box option to the bottom of the list in Testbed.
    Bounding boxes are neat for debugging but not really for demonstrating
    particle types.  Moved the option to render bounding boxes to the bottom of
    the particle parameter list.
    
    Tested:
    Verified bounding boxes is the last option in Testbed on Linux.
    
    Change-Id: I0f842ead0a2cffaeeeeb9591098bcc99f94a79b2

--- liquidfun/Box2D/Testbed/Framework/ParticleParameter.cpp
+++ liquidfun/Box2D/Testbed/Framework/ParticleParameter.cpp
@@ -23,8 +23,6 @@ const ParticleParameter::Value ParticleParameter::k_particleTypes[] =
 	{ b2_waterParticle, ParticleParameter::k_DefaultOptions, "water" },
 	{ b2_waterParticle, ParticleParameter::k_DefaultOptions |
 				ParticleParameter::OptionStrictContacts, "water (strict)" },
-	{ b2_waterParticle, ParticleParameter::k_DefaultOptions |
-				ParticleParameter::OptionDrawAABBs, "water (bounding boxes)" },
 	{ b2_springParticle, ParticleParameter::k_DefaultOptions, "spring" },
 	{ b2_elasticParticle, ParticleParameter::k_DefaultOptions, "elastic" },
 	{ b2_viscousParticle, ParticleParameter::k_DefaultOptions, "viscous" },
@@ -37,6 +35,8 @@ const ParticleParameter::Value ParticleParameter::k_particleTypes[] =
 		"barrier" },
 	{ b2_staticPressureParticle, ParticleParameter::k_DefaultOptions,
 		"static pressure" },
+	{ b2_waterParticle, ParticleParameter::k_DefaultOptions |
+				ParticleParameter::OptionDrawAABBs, "water (bounding boxes)" },
 };
 const ParticleParameter::Value *ParticleParameter::k_particleTypesPtr =
 	ParticleParameter::k_particleTypes;
