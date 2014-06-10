d3f1c96ac5cf02cd33a00afe486cfb848a29c444
Jason Sanmiya <jsanmiya@google.com>
2014-06-10 14:44:57 -0700

    Add porting guide to building-and-running documentation.
    Regenerated documentation.
    
    Bug: 12112366
    Change-Id: I23d32923217938e0fb063ca1e1223adf6726b1dd
    Tested: Displays fine in html. Links work.

--- Building/html/navtreeindex0.js
+++ Building/html/navtreeindex0.js
@@ -1,9 +1,10 @@
 var NAVTREEINDEX0 =
 {
 "index.html":[],
-"md__building_android.html":[0],
-"md__building_linux.html":[1],
-"md__building_o_s_x.html":[2],
-"md__building_windows.html":[3],
+"md__building_android.html":[1],
+"md__building_linux.html":[2],
+"md__building_o_s_x.html":[3],
+"md__building_windows.html":[4],
+"md__porting_from_box2_d.html":[0],
 "pages.html":[]
 };
