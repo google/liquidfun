0076f091de85fb1c9930ac4d0809387f34d1d0ae
Jason Sanmiya <jsanmiya@google.com>
2014-06-18 11:28:59 -0700

    Add build documentation for JavaScript.
    Also, bring back the Makefile, which was inadvertently deleted in
    a previous change.
    
    Tested: testbed.js builds with the steps described.
    Bug: 14447202
    Change-Id: Icca30b3b19409a766edf8346d62d1a6759475cbe

--- Building/html/navtreeindex0.js
+++ Building/html/navtreeindex0.js
@@ -2,6 +2,7 @@ var NAVTREEINDEX0 =
 {
 "index.html":[],
 "md__building_android.html":[1],
+"md__building_java_script.html":[6],
 "md__building_linux.html":[3],
 "md__building_o_s_x.html":[4],
 "md__building_windows.html":[5],
