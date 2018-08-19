479070c77b0e53cc1774b270c8cbb19da758a0be
Haska <haskasu@gmail.com>
2018-08-19 11:26:37 -0400

    bind b2DistanceJoint methods

--- liquidfun/Box2D/lfjs/jsBindings/Dynamics/Joints/b2DistanceJoint.js
+++ liquidfun/Box2D/lfjs/jsBindings/Dynamics/Joints/b2DistanceJoint.js
@@ -1,9 +1,37 @@
+var b2DistanceJointDef_SetLength = Module.cwrap("b2DistanceJointDef_GetLength", "null", ["number", "number"]);
+var b2DistanceJointDef_GetLength = Module.cwrap("b2DistanceJointDef_GetLength", "number", ["number"]);
+var b2DistanceJointDef_SetFrequency = Module.cwrap("b2DistanceJointDef_GetFrequency", "null", ["number", "number"]);
+var b2DistanceJointDef_GetFrequency = Module.cwrap("b2DistanceJointDef_GetFrequency", "number", ["number"]);
+var b2DistanceJointDef_SetDampingRatio = Module.cwrap("b2DistanceJointDef_GetDampingRatio", "null", ["number", "number"]);
+var b2DistanceJointDef_GetDampingRatio = Module.cwrap("b2DistanceJointDef_GetDampingRatio", "number", ["number"]);
+
 /**@constructor*/
 function b2DistanceJoint(def) {
   b2Joint.call(this, def);
 }
 b2DistanceJoint.prototype = Object.create(b2Joint.prototype);
 b2DistanceJoint.prototype.constructor = b2DistanceJoint;
+
+b2DistanceJoint.prototype.SetLength = function(length) {
+    b2DistanceJointDef_SetLength(this.prt, length);
+}
+b2DistanceJoint.prototype.GetLength = function() {
+    return b2DistanceJointDef_GetLength(this.prt);
+}
+
+b2DistanceJoint.prototype.SetFrequency = function(hz) {
+    b2DistanceJointDef_SetFrequency(this.prt, hz);
+}
+b2DistanceJoint.prototype.GetLength = function() {
+    return b2DistanceJointDef_GetLength(this.prt);
+}
+
+b2DistanceJoint.prototype.SetDampingRatio = function(ratio) {
+    b2DistanceJointDef_SetDampingRatio(this.prt, ratio);
+}
+b2DistanceJoint.prototype.GetDampingRatio = function() {
+    return b2DistanceJointDef_GetDampingRatio(this.prt);
+}
 
 var b2DistanceJointDef_Create = Module.cwrap("b2DistanceJointDef_Create",
   'number',
